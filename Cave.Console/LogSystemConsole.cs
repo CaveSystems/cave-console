937e2924c1ef68912ec3a66ec06011f5c9cae697
Andreas Rohleder <andreas@rohleder.cc>
2020-07-30 23:32:27 +0200

    Preparation for 2.0

---
+++ Cave.Console/LogSystemConsole.cs
@@ -0,0 +1,27 @@
+﻿using Cave.Logging;
+
+namespace Cave.Console
+{
+    class LogSystemConsole : ILogTarget
+    {
+        public bool Inverted { get => SystemConsole.Inverted; set => SystemConsole.Inverted = value; }
+
+        public XTColor TextColor { get => SystemConsole.TextColor; set => SystemConsole.TextColor = value; }
+
+        public XTStyle TextStyle { get => SystemConsole.TextStyle; set => SystemConsole.TextStyle = value; }
+
+        public string Title { get => SystemConsole.Title; set => SystemConsole.Title = value; }
+
+        public void Clear() => SystemConsole.Clear();
+
+        public void NewLine() => SystemConsole.NewLine();
+
+        public void ResetColor() => SystemConsole.ResetColor();
+
+        public int Write(XT text) => SystemConsole.Write(text);
+
+        public int Write(XTItem item) => SystemConsole.Write(item);
+
+        public int WriteString(string text) => SystemConsole.WriteString(text);
+    }
+}
